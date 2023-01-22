d6c61693896af7bb8b76c9494741d983d98fc10c
suzuki-hoge <user.ryo@gmail.com>
2023-01-22 19:01:37 +0900

    fix key bind.

--- src/bin/tui/misc/filter/runner.rs
+++ src/bin/tui/misc/filter/runner.rs
@@ -1,11 +1,20 @@
 extern crate bins;
 
+use crossterm::event::read;
 use std::fs::File;
 use std::io;
+use termion::event::Key;
+use termion::get_tty;
+use termion::input::TermRead;
 
 use bins::libs::app::multi_fix_app::MultiFixApp;
-use bins::libs::key::input_filter_dispatcher::dispatch;
+use bins::libs::key::input_filter_dispatcher::Mode::Insert;
+use bins::libs::key::input_filter_dispatcher::{
+    edit, exit, horizontal_move, mode_change, vertical_move, EXIT_KEYS, HORIZONTAL_MOVE_KEYS, MODE_CHANGE_KEYS,
+    VERTICAL_MOVE_KEYS,
+};
 use bins::libs::matcher::string_matcher::Mode;
+use bins::libs::util::tmp_log::tmp_log;
 use tui::backend::CrosstermBackend;
 use tui::Terminal;
 use ui::draw;
@@ -19,5 +28,35 @@ pub fn run(terminal: &mut Terminal<CrosstermBackend<File>>, items: Vec<String>) 
 
     terminal.draw(|frame| draw(frame, &mut app))?;
 
-    dispatch(terminal, Box::new(draw), &mut app)
+    let mut mode = Insert;
+
+    tmp_log(read()?);
+    for key in get_tty()?.keys() {
+        match key.unwrap() {
+            key if HORIZONTAL_MOVE_KEYS.contains(&key) => horizontal_move(&mut app, key),
+            key if VERTICAL_MOVE_KEYS.contains(&key) => vertical_move(&mut app, key),
+            key if MODE_CHANGE_KEYS.contains(&key) => mode = mode_change(&mut app, key),
+            key if EXIT_KEYS.contains(&key) => return exit(&mut app, key),
+
+            // fix one
+            Key::Null => app.fix(),
+
+            // finish
+            Key::Ctrl('f') => return Ok(app.finish()),
+
+            // fix one and finish
+            Key::Char('\n') => {
+                app.fix();
+                return Ok(app.finish());
+            }
+
+            key if mode == Insert => edit(&mut app, key),
+
+            _ => {}
+        }
+
+        terminal.draw(|frame| draw(frame, &mut app))?;
+    }
+
+    Ok(vec![])
 }
--- src/libs/key/input_filter_dispatcher.rs
+++ src/libs/key/input_filter_dispatcher.rs
@@ -9,6 +9,7 @@ use tui::{Frame, Terminal};
 
 use crate::libs::app::multi_fix_app::MultiFixApp;
 use crate::libs::item::previewable_item::PreviewableItem;
+use crate::libs::key::input_filter_dispatcher::Mode::{Insert, Normal};
 
 pub fn dispatch<Item, Drawer>(
     terminal: &mut Terminal<CrosstermBackend<File>>,
@@ -73,4 +74,89 @@ where
     }
 
     Ok(vec![])
+}
+
+pub const HORIZONTAL_MOVE_KEYS: [Key; 6] =
+    [Key::Left, Key::Right, Key::Ctrl('b'), Key::Ctrl('f'), Key::Ctrl('a'), Key::Ctrl('e')];
+
+pub fn horizontal_move<Item>(app: &mut MultiFixApp<Item>, key: Key)
+where
+    Item: PreviewableItem,
+{
+    match key {
+        Key::Left => app.input_app.left(),
+        Key::Right => app.input_app.right(),
+        Key::Ctrl('b') => app.input_app.left(),
+        Key::Ctrl('f') => app.input_app.right(),
+        Key::Ctrl('a') => app.input_app.top(),
+        Key::Ctrl('e') => app.input_app.end(),
+        _ => unreachable!(),
+    }
+}
+
+pub const VERTICAL_MOVE_KEYS: [Key; 4] = [Key::Down, Key::Up, Key::Ctrl('n'), Key::Ctrl('p')];
+
+pub fn vertical_move<Item>(app: &mut MultiFixApp<Item>, key: Key)
+where
+    Item: PreviewableItem,
+{
+    match key {
+        Key::Down => app.scrolling_select_app.down(),
+        Key::Up => app.scrolling_select_app.up(),
+        Key::Ctrl('n') => app.scrolling_select_app.down(),
+        Key::Ctrl('p') => app.scrolling_select_app.up(),
+        _ => unreachable!(),
+    }
+}
+
+#[derive(Eq, PartialEq)]
+pub enum Mode {
+    Normal,
+    Insert,
+}
+
+pub const MODE_CHANGE_KEYS: [Key; 2] = [Key::Esc, Key::Char('i')];
+
+pub fn mode_change<Item>(_: &mut MultiFixApp<Item>, key: Key) -> Mode
+where
+    Item: PreviewableItem,
+{
+    match key {
+        Key::Esc => Normal,
+        Key::Char('i') => Insert,
+        _ => unreachable!(),
+    }
+}
+
+pub const EXIT_KEYS: [Key; 1] = [Key::Ctrl('c')];
+
+pub fn exit<Item>(_: &mut MultiFixApp<Item>, key: Key) -> io::Result<Vec<Item>>
+where
+    Item: PreviewableItem,
+{
+    match key {
+        Key::Ctrl('c') => Ok(vec![]),
+        _ => unreachable!(),
+    }
+}
+
+pub fn edit<Item>(app: &mut MultiFixApp<Item>, key: Key)
+where
+    Item: PreviewableItem,
+{
+    match key {
+        Key::Char(c) => {
+            app.input_app.insert(c);
+            app.refresh();
+        }
+        Key::Backspace => {
+            app.input_app.remove();
+            app.refresh();
+        }
+        Key::Ctrl('k') => {
+            app.input_app.cut();
+            app.refresh();
+        }
+        _ => {}
+    }
 }
